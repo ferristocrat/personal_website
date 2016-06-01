22d666e4f985af1e545828244d279bec5d48f10a
ferristocrat <michael@ferristocrat.com>
2016-06-01 00:57:15 -0400

    feat: responsive layout
    Added responsiveness to the layout.  Began work on mobile menu, but need
    to complete this task

--- main.js
+++ main.js
@@ -3,15 +3,28 @@
 
   app.controller('PageController', function() {
     this.page = 1;
-    console.log("Page is set to: "+1);
 
     this.isSet = function(checkPage) {
       return this.page === checkPage;
-    }
+    };
 
     this.setPage = function(setPage) {
       this.page = setPage;
       console.log("Page is set to: "+setPage);
+    };
+
+    this.minimenu = 1;
+
+    this.menuState = function(state) {
+      return this.minimenu === state;
+    };
+
+    this.navOpen = function() {
+      this.minimenu = 2;
+    };
+
+    this.navClose = function() {
+      this.minimenu = 1;
     };
   });
 })();
