0d76fc2f0c900d90e9040bd75782e6130be31b99
ferristocrat <m.c.ferris@me.com>
2016-06-08 22:05:48 -0400

    feat: completed mobile navigation

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 
     this.setPage = function(setPage) {
       this.page = setPage;
-      console.log("Page is set to: "+setPage);
+      this.navClose();
     };
 
     this.minimenu = 1;
@@ -20,9 +20,6 @@
     };
 
     this.screenSize = function() {
-      console.log('screen size run');
-      console.log('');
-      console.log(screen.width);
       return screen.width < 680;
     }
 
@@ -40,8 +37,7 @@
   });
 
   var blog = [{
-    title: 'Forward Momentum',
-    description: 'Why I rebulit my personal website',
+    title: 'New Personal Site',
     content: "Early this year I learned Angular.js but haven't really had the time to put my knowledge to the test. While a bit overkill to use Angular for my personal site, it's been fun and quick to implement.  Furthermore, I wanted something that was all my own code, so the only source that's not my own is the angular.js file.  I decided to ditch bootstrap since most the components just aren't needed for such a simple site.  As I build this out, I'll be able to add new modules and develop my own css framework to suit my needs.  I have a few more features to finish for this build, but then plan to continue iterating on the old and building new things.  I hope to write about my progress as a way of reflection as well as to maintain forward momentum with all the things I'm passionate about.  To start I'll write about my coding, but I also want to write about my training for a 125 mile bike ride later this year among other things.",
     upnext: "This 'blog' currently just pulls from a static json file, where I have typed the info for the different sections.  The next bit of coding I want to get done is a revamp of the blog's design as well as a more streamlined way to input the info such as a private webform connected to an AWS instance or Google App Engine backend where the data can be stored.",
     date: 1464926499287
